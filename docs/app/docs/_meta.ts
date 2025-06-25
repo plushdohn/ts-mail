3de4ffb56f8a1706876d19b734d1ef7acd9f3f7d
Luca Farci <luca.farci@external.facile.it>
2025-06-25 23:31:32 +0200

    chore: draft framework guides

---
+++ docs/app/docs/_meta.ts
@@ -0,0 +1,15 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "getting-started": {
+    title: "Getting started",
+  },
+  styling: {
+    title: "Styling",
+  },
+  "framework-guides": {
+    title: "Framework guides",
+  },
+};
+
+export default meta;
