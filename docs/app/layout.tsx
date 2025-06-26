75c91cb1b37cd8d43351a7de2569ffd5a62d0ab4
Luca Farci <luca.farci@external.facile.it>
2025-06-26 17:48:00 +0200

    feat(docs): meta title and wording on styling

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,10 +3,18 @@ import { getPageMap } from "nextra/page-map";
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
 
 import "./global.css";
+import { Metadata } from "next";
 
 const navbar = <Navbar logo={<b>ts-mail</b>} />;
 
 const footer = <Footer>MIT {new Date().getFullYear()} © ts-mail</Footer>;
+
+export const metadata: Metadata = {
+  title: {
+    default: "ts-mail - Simple Typescript Emails",
+    template: "%s - ts-mail documentation",
+  },
+};
 
 export default async function RootLayout({
   children,
